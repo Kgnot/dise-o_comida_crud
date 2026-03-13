2b9f2f5c8af9474a4c0178e3e96f7f348b2e9d5a
kgnot <hricaurtem@proton.me>
2026-03-12 19:02:48 -0500

    refactorización completa y visual de swing

--- src/main/java/proj/food/MainApp.java
+++ src/main/java/proj/food/MainApp.java
@@ -1,14 +1,22 @@
 package proj.food;
 
+import com.formdev.flatlaf.intellijthemes.FlatLightFlatIJTheme;
 import proj.food.config.app_context.ApplicationContext;
 import proj.food.vista.ViewType;
 import proj.food.vista.implementation.fx.FxRuntime;
 import proj.food.vista.implementation.fx.MainShellFX;
+import proj.food.vista.implementation.swing.MainShell;
+
+import javax.swing.*;
 
 public class MainApp {
 
     public static void main(String[] args) {
         ApplicationContext.getInstance();
         FxRuntime.runOnFxThread(() -> new MainShellFX().show(ViewType.START));
+        FlatLightFlatIJTheme.setup();
+        SwingUtilities.invokeLater(() -> new MainShell().show(ViewType.START));
+
     }
+
 }
